da271053e7049ae41cd558146df68eb049ef510f
Sir-Mr-Bman <heybrennan@gmail.com>
2014-07-20 03:37:13 -0400

    Added source code... fml

---
+++ src/net/mcdirect/plugins/lobby/Header.java
@@ -0,0 +1,28 @@
+package net.mcdirect.plugins.lobby;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * MCDirect_ Lobby Essentials
+ * Copyright (C) Sir_Mr_Bman and MCDirect.net
+ * ALL RIGHTS RESERVED
+ * Please see the license file on our web-site for more:
+ * www.mcdirect.net/license
+ */
+
+public class Header extends JavaPlugin {
+	
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void log (String msg) { this.logger.info("[LobbyEssentials] " + msg); }
+}
